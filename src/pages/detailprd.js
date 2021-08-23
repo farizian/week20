f30809f23f291737973c735b6fc17a7e1c402d59
farizian <farizirfan8@gmail.com>
2021-08-23 21:39:03 +0700

    update react api

--- src/pages/detailprd.js
+++ src/pages/detailprd.js
@@ -1,73 +1,153 @@
 /* eslint-disable array-callback-return */
+import { useState, useEffect } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import '../css/detail/body.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import {useHistory} from 'react-router-dom'
 import CurrencyFormat from 'react-currency-format'
+import axios from "axios"
 
 const Detail =(props)=>{
-const local = localStorage.getItem('data')
-const prd = JSON.parse(local)
+const [toggle, setToggle]=useState()
+const [prd, setPrd]=useState([])
+const history = useHistory();
+const token = localStorage.getItem('token')
 const id = props.match.params.id
-const cart = []
-// eslint-disable-next-line array-callback-return
-const product = prd.find((e)=>{
-  // eslint-disable-next-line eqeqeq
-  if(e.id==id){
-    return e
-  }
+const [updData, setUpd]=useState({
+  img: "",
+  disc: "",
+  name: "",
+  price: "",
+  category: ""
 })
-const sizebtn =(data)=>{
-  if(cart.length<=0){
-    cart.push({
-      size: data,
-      qty: 1})
-  }else{
-    cart.push(...cart,{size: data})
-  }
-}
-const delivbtn =(data)=>{
-  if(cart.length<=0){
-    cart.push({delivery: data})
-  }else{
-    cart.push(...cart,{delivery: data})
-  }
-}
-const setTime=(event)=>{
-  if(cart.length<=0){
-    const time = event.target.value
-    cart.push(time)
-  }else{
-    const time = event.target.value
-    cart.push(...cart,{time})
-  }
-}
-const plus=()=>{
-  const find = cart.filter((e)=>{
-    e.qty +=1
+const getData =()=>{
+  axios.get(`${process.env.REACT_APP_URL_PRODUCT}/${id}`, {headers: {token: token} })
+  .then((response)=>{
+    setPrd(response.data.field)
+    console.log(response.data.field)
+  }).catch((err)=>{
+    console.log(err)
   })
-  cart.push(find)
 }
-const min=()=>{
-  const find = cart.filter((e)=>{
-    e.qty -=1
+useEffect(()=>{
+  getData()
+// eslint-disable-next-line react-hooks/exhaustive-deps
+},[])
+const clicked=(data)=>{
+  setToggle(data)
+}
+// update
+const addImg=(event)=>{
+  setUpd({
+    img: event.target.value
   })
-  cart.push(find)
+}
+const addDisc=(event)=>{
+  setUpd({
+    ...updData,
+    disc: event.target.value
+  })
+}
+const addName=(event)=>{
+  setUpd({
+    ...updData,
+    name: event.target.value
+  })
+}
+const addPrice=(event)=>{
+  setUpd({
+    ...updData,
+    price: event.target.value
+  })
+}
+const addCategory=(event)=>{
+  setUpd({
+    ...updData,
+    category: event.target.value
+  })
 }
 const inputData=async()=>{
   try{
-    const input1 = await sizebtn()
-    const input2 = await delivbtn(input1)
-    const input3 = await setTime(input2)
-    return input3
+    const input1 = await addImg()
+    const input2 = await addDisc(input1)
+    const input3 = await addName(input2)
+    const input4 = await addPrice(input3)
+    const input5 = await addCategory(input4)
+    return input5
   }catch(error){
     console.log(error)
   }
 }
-const addqty=()=>{
+const insertPrd=(event)=>{
   inputData()
-  plus()
+  event.preventDefault();
+  console.log(updData)
+  const {img, disc, name, price, category}=updData
+  const body={
+    idStyle: `prd${Math.random()}`,
+    img: img===undefined?"":img,
+    disc: disc===undefined?"":disc,
+    prdname: name===undefined?"":name,
+    price: price===undefined?"":price,
+    category_id: category==="main course"?1:category===undefined?"":2,
+    qty: 1
+  }
+  const headers={
+    headers: {
+      token: token
+    }
+  }
+  axios.post(`${process.env.REACT_APP_URL_PRODUCT}`, body, headers)
+  .then((response)=>{
+    getData()
+    alert("input data berhasil")
+    setUpd({
+      img: "",
+      disc: "",
+      name: "",
+      price: "",
+      category: ""
+    })
+    history.push("/product");
+  }).catch((err)=>{
+    console.log(err)
+  })
 }
+const updatePrd=(event)=>{
+  inputData()
+  event.preventDefault();
+  console.log(updData)
+  const {img, disc, name, price, category}=updData
+  const body={
+    img: img===undefined?"":img,
+    disc: disc===undefined?"":disc,
+    prdname: name===undefined?"":name,
+    price: price===undefined?"":price,
+    category_id: category==="Main Course"?1:category===undefined?"":2
+  }
+  const headers={
+    headers: {
+      token: token
+    }
+  }
+  axios.put(`${process.env.REACT_APP_URL_PRODUCT}/${id}`, body, headers)
+  .then((response)=>{
+    getData()
+    alert("Update data berhasil")
+    setUpd({
+      img: "",
+      disc: "",
+      name: "",
+      price: "",
+      category: ""
+    })
+    history.push("/product");
+  }).catch((err)=>{
+    console.log(err)
+  })
+}
+
   return(
     <div>
       <Navbar logsign={false} product={true}/>
@@ -75,69 +155,127 @@ const addqty=()=>{
         <div className="row" id="dtlrow1">
           <div className="col-lg-5 col-12 viewprd">
             <div className="txt">
-              <p className="textdtl" id="p1">Favorite & Promo</p>
-              <p className="textdtl" id="p2" >{">"} Cold Brew</p>
+              <p className="textdtl" id="p1" onClick={()=>clicked('prd')}>Favorite & Promo</p>
+              <p className="textdtl" id="p2" >{`> ${prd.map((e)=>(e.prdname))}`}</p>
             </div>
-            <div className="dtlproduct">
-              <img className="prdimg" src={product.img} alt=""/>
-              <h1 className="prdname">{product.prdname.toUpperCase()}</h1>
-              <CurrencyFormat className="prdprice" value={product.price} displayType={'text'} thousandSeparator={true} prefix={'Rp. '}/>
-            </div>
+            {prd.map((e)=>{
+              return(
+              <div className="dtlproduct">
+                <img className="prdimg" src={e.img} alt=""/>
+                <h1 className="prdname">{e.prdname.toUpperCase()}</h1>
+                <CurrencyFormat className="prdprice" value={e.price} displayType={'text'} thousandSeparator={true} prefix={'Rp. '}/>
+              </div>
+              )
+            })}
             <div className="dtlbutton">
-              <button className="btn" id="btn1">Add to Cart</button>
-              <button className="btn">Ask a Staff</button>
+              <button className="btn" id="btn1" onClick={()=>clicked('update')}>Update Product</button>
+              <button className="btn" onClick={()=>clicked('add')}>Add Product</button>
             </div>
           </div>
+          {toggle==="update"?
           <div className="col-lg-7 col-12 desc">
-            <div className="descbox">
-              <div className="textbox">
-                <p className="text" id="text1">Delivery only on <b>Monday to friday at  1 - 7 pm</b></p>
-                <p className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <div className="descbox2">
+              <form className="insert" onSubmit={updatePrd}>
+                <div className="textbox" id="txtbox1">
+                  <h3>Product Img Link :</h3>
+                  <input type="text" placeholder="Enter your img address" name="" onChange={addImg}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Discount :</h3>
+                  <input type="text" placeholder="Enter your discount" name="" onChange={addDisc}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Product Name :</h3>
+                  <input type="text" placeholder="Enter product name" name="" onChange={addName}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Price :</h3>
+                  <input type="number" placeholder="Enter your price" name="" onChange={addPrice}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Category :</h3>
+                  <input type="text" placeholder="Enter category" name="" onChange={addCategory}></input>
+                </div>
+                <button onClick={updatePrd}>submit</button>
+              </form>
+            </div>
+          </div>:
+          toggle==="add"?
+          <div className="col-lg-7 col-12 desc">
+            <div className="descbox2">
+              <form className="insert" onSubmit={insertPrd}>
+                <div className="textbox" id="txtbox1">
+                  <h3>Product Img Link :</h3>
+                  <input type="text" placeholder="Enter your img address" name="" onChange={addImg}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Discount :</h3>
+                  <input type="text" placeholder="Enter your discount" name="" onChange={addDisc}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Product Name :</h3>
+                  <input type="text" placeholder="Enter product name" name="" onChange={addName}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Price :</h3>
+                  <input type="number" placeholder="Enter your price" name="" onChange={addPrice}></input>
+                </div>
+                <div className="textbox">
+                  <h3>Category :</h3>
+                  <input type="text" placeholder="Enter category" name="" onChange={addCategory}></input>
+                </div>
+                <button onClick={insertPrd}>submit</button>
+              </form>
+            </div>
+          </div>:
+            <div className="col-lg-7 col-12 desc">
+              <div className="descbox">
+                <div className="textbox">
+                  <p className="text" id="text1">Delivery only on <b>Monday to friday at  1 - 7 pm</b></p>
+                  <p className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div className="sizemenu">
+                  <p className="text">Choose a size</p>
+                  <div className="sizebox">
+                    <div onClick="" className="size">R</div>
+                    <div onClick="" className="size">L</div>
+                    <div onClick="" className="size" id="xl">XL</div>
+                  </div>
+                </div>
               </div>
-              <div className="sizemenu">
-                <p className="text">Choose a size</p>
-                <div className="sizebox">
-                  <div onClick={()=>sizebtn('(Regular)')} className="size">R</div>
-                  <div onClick={()=>sizebtn('(Large)')} className="size">L</div>
-                  <div onClick={()=>sizebtn('(Xtra Large)')} className="size" id="xl">XL</div>
+              <div className="delivery">
+                <p className="txtmenu">Choose Delivery Methods</p>
+                <div className="buttonbox">
+                  <button onClick="" className="btn">Dine in</button>
+                  <button onClick="" className="btn" id="deliv2">Door Delivery</button>
+                  <button onClick="" className="btn">Pick up</button>
+                </div>
+                <div className="inputbox">
+                  <p className="text">Set time :</p>
+                  <input onChange="" className="input" placeholder="Enter the time you’ll arrived"></input>
                 </div>
               </div>
             </div>
-            <div className="delivery">
-              <p className="txtmenu">Choose Delivery Methods</p>
-              <div className="buttonbox">
-                <button onClick={()=>delivbtn('Dine in')} className="btn">Dine in</button>
-                <button onClick={()=>delivbtn('Door Delivery')} className="btn" id="deliv2">Door Delivery</button>
-                <button onClick={()=>delivbtn('Pick up')} className="btn">Pick up</button>
-              </div>
-              <div className="inputbox">
-                <p className="text">Set time :</p>
-                <input onChange={setTime} className="input" placeholder="Enter the time you’ll arrived"></input>
-              </div>
-            </div>
-          </div>
+          }
         </div>
       </div>
       <div className="dtlprdcard">
-        <div className="card">
-          <img src={product.img} alt="">
-          </img>
-          <div className="text">
-            <h1 className="header">{product.prdname.toUpperCase()}</h1>
-            {cart.map((e)=>{
-              return(
-                <div>
-                <p className="txt">{`x${e.qty} ${e.size}`}</p>
-                </div>
-              )
-            })}
-          </div>
-          <div className="qtybtn">
-            <button onClick={min} className="btn">-</button>
-            <p className="qty">{cart.map((e)=>(e.qty))}</p>
-            <button onClick={addqty} className="btn">+</button>
-          </div>
-        </div>
+          {prd.map((e)=>{
+            return(
+            <div className="card">
+              <img src={e.img} alt="">
+              </img>
+              <div className="text">
+                <h1 className="header">{e.prdname.toUpperCase()}</h1>
+              </div>
+              <div className="qtybtn">
+                <button onClick="" className="btn">-</button>
+                <p className="qty">1</p>
+                <button onClick="" className="btn">+</button>
+              </div>
+            </div>
+            )
+          })}
         <button className="btncard">CHECKOUT</button>
       </div>
       <Footer/>
--- src/pages/product.js
+++ src/pages/product.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import {Link, useHistory, useLocation} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import "../css/product/section.css"
 import "../css/product/aside.css"
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -11,14 +11,7 @@ import axios from "axios"
 
 const Product=()=>{
 const [localdata, setLocal]=useState([])
-const [updData, setUpd]=useState({
-  img: "",
-  disc: "",
-  name: "",
-  price: "",
-  category: ""
-})
-const [toggle, setToggle]=useState()
+const [search, setSearch]=useState("")
 const token = localStorage.getItem('token')
 const getData =()=>{
   axios.get(`${process.env.REACT_APP_URL_PRODUCT}?field=id`, {headers: {token: token} })
@@ -32,78 +25,7 @@ useEffect(()=>{
   getData()
 // eslint-disable-next-line react-hooks/exhaustive-deps
 },[])
-const navproduct=(data)=>{
-  setToggle(data)
-}
-const addImg=(event)=>{
-  setUpd({
-    img: event.target.value
-  })
-}
-const addDisc=(event)=>{
-  setUpd({
-    ...updData,
-    disc: event.target.value
-  })
-}
-const addName=(event)=>{
-  setUpd({
-    ...updData,
-    name: event.target.value
-  })
-}
-const addPrice=(event)=>{
-  setUpd({
-    ...updData,
-    price: event.target.value
-  })
-}
-const addCategory=(event)=>{
-  setUpd({
-    ...updData,
-    category: event.target.value
-  })
-}
-const inputData=async()=>{
-  try{
-    const input1 = await addImg()
-    const input2 = await addDisc(input1)
-    const input3 = await addName(input2)
-    const input4 = await addPrice(input3)
-    const input5 = await addCategory(input4)
-    return input5
-  }catch(error){
-    console.log(error)
-  }
-}
 
-const insertPrd=(event)=>{
-  inputData()
-  event.preventDefault();
-  console.log(updData)
-  const {img, disc, name, price, category}=updData
-  const body={
-    img: img,
-    disc: disc,
-    prdname: name,
-    price: price,
-    category_id: category,
-    qty: 1
-  }
-  const headers={
-    headers: {
-      token: token
-    }
-  }
-  axios.post(`${process.env.REACT_APP_URL_PRODUCT}`, body, headers)
-  .then((response)=>{
-    getData()
-    alert("input data berhasil")
-    setToggle("product")
-  }).catch((err)=>{
-    console.log(err)
-  })
-}
 const deletePrd=(id)=>{
   const headers={
     headers: {
@@ -111,153 +33,25 @@ const deletePrd=(id)=>{
     }
   }
   axios.delete(`${process.env.REACT_APP_URL_PRODUCT}/${id}`,headers)
-  .then((response)=>{
-    getData()
-  })
+  .then(getData())
   .catch((err)=>{
     console.log(err)
   })
 }
-const product = [
-{
-  id: 1,
-  idStyle: "prd1",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/sayur.png",
-  disc: "10%",
-  prdname: "Veggie tomato mix",
-  price: 34000,
-  qty: 1
-},
-{
-  id: 2,
-  idStyle: "prd2",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/hazelnut%20latte.png",
-  disc: "",
-  prdname: "Hazelnut Latte",
-  price: 25000,
-  qty: 1
-},
-{
-  id: 3,
-  idStyle: "prd3",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/summer%20fried%20rice.png",
-  disc: "13%",
-  prdname: "Summer Fried Rice",
-  price: 32000,
-  qty: 1
-},
-{
-  id: 4,
-  idStyle: "prd4",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/creamy.png",
-  disc: "",
-  prdname: "Creamy Ice Latte",
-  price: 27000,
-  qty: 1
-},
-{
-  id: 5,
-  idStyle: "prd5",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/drum%20stick.png",
-  disc: "20%",
-  prdname: "Drum Stick",
-  price: 34000,
-  qty: 1
-},
-{
-  id: 6,
-  idStyle: "prd6",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/salty%20rice.png",
-  disc: "10%",
-  prdname: "Salty Rice",
-  price: 34000,
-  qty: 1
-},
-{
-  id: 7,
-  idStyle: "prd7",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/summer%20fried%20rice.png",
-  disc: "13%",
-  prdname: "Summer Fried Rice",
-  price: 32000,
-  qty: 1
-},
-{
-  id: 8,
-  idStyle: "prd8",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/creamy.png",
-  disc: "",
-  prdname: "Creamy Ice Latte",
-  price: 27000,
-  qty: 1
-},
-{
-  id: 9,
-  idStyle: "prd9",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/sayur.png",
-  disc: "10%",
-  prdname: "Veggie tomato mix",
-  price: 34000,
-  qty: 1
-},
-{
-  id: 10,
-  idStyle: "prd10",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/hazelnut%20latte.png",
-  disc: "",
-  prdname: "Hazelnut Latte",
-  price: 25000,
-  qty: 1
-},
-{
-  id: 11,
-  idStyle: "prd11",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/summer%20fried%20rice.png",
-  disc: "13%",
-  prdname: "Summer Fried Rice",
-  price: 32000,
-  qty: 1
-},
-{
-  id: 12,
-  idStyle: "prd12",
-  img: "https://raw.githubusercontent.com/farizian/week5/master/img/creamy.png",
-  disc: "",
-  prdname: "Creamy Ice Latte",
-  price: 27000,
-  qty: 1
-}]
-localStorage.setItem('data', JSON.stringify(product))
-const local = localStorage.getItem('data')
-const prd = JSON.parse(local)
-const location= useLocation()
-const history = useHistory()
-const [search, setSearch]=useState("")
-const query = new URLSearchParams(location.search)
-const hasilSearch = query.get("search")
 
-const searchGet=(search)=>{
-  setSearch(search)
-  history.push(`/product?search=${search}`)
+const searchPrd=(data)=>{
+  setSearch(data)
+  axios.get(`${process.env.REACT_APP_URL_PRODUCT}?search=${search}&field=id`, {headers: {token: token} })
+  .then((response)=>{
+    setLocal(response.data.field.data)
+  }).catch((err)=>{
+    console.log(err)
+  })
 }
-useEffect(()=>{
-  if(hasilSearch && hasilSearch !== ""){
-    // eslint-disable-next-line array-callback-return
-    const prdSearch = prd.filter((e)=>{
-      if(e.prdname === hasilSearch){
-        return e
-      }
-    })
-    setLocal(prdSearch)
-  }else{
-    setLocal(prd)
-  }
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, [search])
 
   return(
     <div>
-      <Navbar logsign={false} product={true} search={searchGet}/>
+      <Navbar logsign={false} product={true} search={searchPrd}/>
       <main className="prdmain">
         <aside className="prdaside">
           <div className="header">
@@ -312,46 +106,22 @@ useEffect(()=>{
         <section className="productitem">
           <nav className="prdmenu">
             <ul className="navprd">
-              <li onClick={()=>navproduct("product")}>Favorite & Promo</li>
-              <li onClick={()=>navproduct("addprd")}>Add Product</li>
+              <li>Favorite & Promo</li>
+              <li>Add Product</li>
               <li>Edit Product</li>
               <li>Foods</li>
               <li>Add-on</li>
             </ul>
           </nav>
           <div className="row">
-            {toggle==="addprd"?
-            <form className="insert" onSubmit={insertPrd}>
-            <div className="textbox" id="txtbox1">
-              <h3>Product Img Link :</h3>
-              <input type="text" placeholder="Enter your img address" name="" onChange={addImg}></input>
-            </div>
-            <div className="textbox">
-              <h3>Discount :</h3>
-              <input type="text" placeholder="Enter your discount" name="" onChange={addDisc}></input>
-            </div>
-            <div className="textbox">
-              <h3>Product Name :</h3>
-              <input type="text" placeholder="Enter product name" name="" onChange={addName}></input>
-            </div>
-            <div className="textbox">
-              <h3>Price :</h3>
-              <input type="number" placeholder="Enter your price" name="" onChange={addPrice}></input>
-            </div>
-            <div className="textbox">
-              <h3>Category :</h3>
-              <input type="number" placeholder="Enter category" name="" onChange={addCategory}></input>
-            </div>
-            <button onClick={insertPrd}>submit</button>
-          </form>:
-            localdata.map((e, i)=>{
+            {localdata.map((e, i)=>{
               return(
                 <div key={i} className="card col-lg-2 col-md-4 col-6" id={e.idStyle}>
                   <button onClick={()=>deletePrd(e.id)} className="remove">-</button>
                   <Link className="link" to={`/detailprd/${e.id}`}>
                   <img className="prdimg" src={e.img} alt=""/>
-                  {e.disc!==null?
-                  <p className="disc">{e.disc}</p>:null
+                  {e.disc===null?
+                  null:e.disc===""?null:<p className="disc">{e.disc}</p>
                   }
                   <p className="prdname">{e.prdname}</p>
                   <CurrencyFormat className="harga" value={e.price} displayType={'text'} thousandSeparator={true} prefix={'Rp. '}/>
